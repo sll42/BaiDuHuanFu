692d2e0c2220fc037c8a597f83996fd065f7bfab
sll42 <1910943723@qq.com>
2019-03-27 17:16:24 +0800

    提交所有

---
+++ js/index.js
@@ -0,0 +1,18 @@
+	window.onload=function(){
+				var imgs=document.getElementsByTagName("img");
+			imgs[0].onclick=function(){
+				document.body.style.backgroundImage="url(img/1.jpg)";
+			}              
+			imgs[1].onclick=function(){
+				document.body.style.backgroundImage="url(img/2.jpg)";
+			}
+			imgs[2].onclick=function(){
+				document.body.style.backgroundImage="url(img/3.jpg)";
+			}
+			imgs[3].onclick=function(){
+				document.body.style.backgroundImage="url(img/4.jpg)";
+			}
+			imgs[4].onclick=function(){
+				document.body.style.backgroundImage="url(img/5.jpg)";	
+			}
+		}
